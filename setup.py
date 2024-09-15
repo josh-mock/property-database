b8fc8b3f3c0d89c8ea89dbfbd18c540a9dace299
jwmock88 <166959561+jwmock88@users.noreply.github.com>
2024-09-15 13:22:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from setuptools import setup, find_packages
-from __version__ import __version__
-
-def read_requirements(filename):
-    """Read requirements from a file"""
-    with open(filename, 'r') as f:
-        return [line.strip() for line in f.readlines() if line.strip() and not line.startswith('#')]
 
 setup(
     name='property_project',
-    version=__version__,  # Use the version imported from scripts.__version__
-    packages=find_packages(where='scripts'),
-    package_dir={'': 'scripts'},
+    version='1.0.0',
+    packages=find_packages(),  # Automatically find packages in the current directory
     entry_points={
         'console_scripts': [
-            'build=scripts.build:main',  # Adjust if main() is defined in build.py
-            'search=scripts.search:main',  # Adjust if main() is defined in search.py
+            'build=build:main',
+            'search=search:main',
         ],
     },
-    install_requires=['pandas', 'tabulate', 'requests', 'pyarrow', 'maskpass', 'fpdf2'],
-
+    install_requires=[
+        'pandas', 'tabulate',
+        'requests', 'pyarrow', 'maskpass', 'fpdf2'
+    ],
     author='Josh Mock',
     description='This project builds a database for searching the CCOD and OCOD datasets released by the UK Land Registry (https://use-land-property-data.service.gov.uk/).',
     long_description=open('README.md').read(),
